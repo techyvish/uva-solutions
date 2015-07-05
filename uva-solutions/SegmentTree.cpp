bbfed91907db9882ce18d531f436de59a001be9b
Vishal Patel <vis8051@gmail.com>
2015-07-06 02:10:56 +1000

    Segment treet

--- uva-solutions/SegmentTree.cpp
+++ uva-solutions/SegmentTree.cpp
@@ -45,13 +45,76 @@ private:
     int left (int p) {
         return  p << 1;
     }
+
     int right (int p) {
         return  (p << 1) + 1;
     }
 
     void buid(int p, int L, int R) {
+        if ( L == R )
+        {
+            st[p] = L;
+        }
+        else
+        {
+            int p1 = left(p);
+            int p2 = right(p);
+
+            buid(p1, L              , (L + R) / 2);
+            buid(p2, (L + R) / 2 + 1,           R);
+
+            p1 = st[left(p)];
+            p2 = st[right(p)];
+
+            st[p] = (A[p1] <= A[p2]) ? p1 : p2;
+        }
+    }
+
+    int rmq( int p, int L, int R, int i, int j) {
+
+        if (i > R || j < L) return -1;
+        if (L >= i && R <= j) return st[p];
+
+        int p1 = rmq(left(p) , L          , (L + R) / 2, i, j);
+        int p2 = rmq(right(p), (L+R)/2+1  ,           R, i, j);
+
+        if ( p1 == -1) return p2;
+        if ( p2 == -1) return p1;
+
+        return  ( A[p1] <= A[p2]) ? p1 : p2;
 
     }
 
+public:
+    SegmentTree(const vi &_A)
+    {
+        A = _A;
+        n = (int) A.size();
+        st.assign(4*n, 0);
+        buid(1, 0, n-1);
+    }
 
+    int rmq(int i,int j)
+    {
+        return rmq(1,0,n-1,i,j);
+    }
 };
+
+int main()
+{
+    int arr[]= {18,17,13,19,15,11,20};
+    //vi A(arr,arr+7);
+    vi A;
+    A.push_back(18);
+    A.push_back(17);
+    A.push_back(13);
+    A.push_back(19);
+    A.push_back(15);
+    A.push_back(11);
+    A.push_back(20);
+
+    SegmentTree s(A);
+
+    cout << s.rmq(1,3);
+    return 0;
+}
